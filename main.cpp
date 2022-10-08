8c88f106488b3a2fffea6be39250d3f020e5bb88
Ali_Mohammed 20210876 <53825610+aliCFE@users.noreply.github.com>
2022-10-08 15:45:43 +0300

    Adding (f) +  making user choosing the numbers

--- main.cpp
+++ main.cpp
@@ -7,6 +7,59 @@ class BigDecimalInt
 {
     string num;
 
+    //ahmed shaban code
+public:
+bool operator>(BigDecimalInt anotherDec){
+        if(anotherDec.num[0] == '-' and num[0] == '+'){
+            return true;
+        }else if(anotherDec.num[0] == '+' and num[0] == '+'){
+            if(anotherDec.num.size() > num.size()){
+                return false;
+            }
+            else if(anotherDec.num.size() < num.size()){
+                return true;
+            }else{
+                for(int i = 0; i < num.size();){
+                    if(num[i] == anotherDec.num[i]){
+                        i++;
+                    }
+                    else{
+                        if(num[i] > anotherDec.num[i]){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                }
+            }
+        }else if (anotherDec.num[0] == '+' and num[0] == '-'){
+            return false;
+        }else if(anotherDec.num[0] == '-' and num[0] == '-'){
+            if(anotherDec.num.size() > num.size()){
+                return true;
+            }
+            else if(anotherDec.num.size() < num.size()){
+                return false;
+            }else{
+                for(int i = 0; i < num.size();){
+                    if(num[i] == anotherDec.num[i]){
+                        i++;
+                    }
+                    else{
+                        if(num[i] > anotherDec.num[i]){
+                            return false;
+                        }
+                        else{
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
     // Ali El Iraqi Code
 public:
     bool operator<(BigDecimalInt anotherDec)
@@ -50,6 +103,27 @@ public:
         else if (anotherDec.num[0] == '+' && num[0] == '-')
         {
             return true;
+        }else if(anotherDec.num[0] == '-' and num[0] == '-'){
+            if(anotherDec.num.size() > num.size()){
+                return false;
+            }
+            else if(anotherDec.num.size() < num.size()){
+                return true;
+            }else{
+                for(int i = 0; i < num.size();){
+                    if(num[i] == anotherDec.num[i]){
+                        i++;
+                    }
+                    else{
+                        if(num[i] > anotherDec.num[i]){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -107,12 +181,24 @@ private:
 
 int main()
 {
-    BigDecimalInt bigNumber("-4");
-    BigDecimalInt smallNumber("-5");
+    string BigDecimalInt1, BigDecimalInt2;
+    cout << "Enter the first BigDecimalInt: ";
+    cin >> BigDecimalInt1;
+
+    cout << "Enter the second BigDecimalInt: ";
+    cin >> BigDecimalInt2;
+
+    BigDecimalInt bigNumber(BigDecimalInt1);
+    BigDecimalInt smallNumber(BigDecimalInt2);
 
     // BigDecimalInt firstNumber("-1036444444444463464363468");
     // BigDecimalInt secondNumber("20834643634634643643643436436");
     // BigDecimalInt sumNumber = firstNumber + secondNumber;
     // cout << sumNumber.getNum();
-    cout << (bigNumber < smallNumber);
+    if(bigNumber < smallNumber){
+        cout << BigDecimalInt2 << endl << "is greater than" << endl << BigDecimalInt1;
+    }
+    else if(bigNumber > smallNumber){
+        cout << BigDecimalInt1 << endl << "is greater than" << endl << BigDecimalInt2;
+    }
 }
