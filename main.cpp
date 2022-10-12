6dbc410a335644ed2c632f0afd0a3e28ffab129b
Youssef Emad <usf3md@gmail.com>
2022-10-12 21:19:53 +0200

    Fixed + operator and added - operator

--- main.cpp
+++ main.cpp
@@ -90,8 +90,6 @@ public:
 
     // Ali El Iraqi Code <
 public:
-    // Ali El Iraqi Code <
-public:
     bool operator<(BigDecimalInt anotherDec)
     {
         if (anotherDec.signChar == '-' && signChar == '+')
@@ -167,7 +165,12 @@ public:
             }
         }
     }
-
+    BigDecimalInt operator-(BigDecimalInt anotherDec)
+    {
+        anotherDec.signInt *= -1;
+        anotherDec.signChar = ((anotherDec.signChar == '+') ? '-' : '+');
+        return *this + anotherDec;
+    }
     // USF EMAD CODE
 public:
     BigDecimalInt(string DecStr) // Overload for string inputs
@@ -189,7 +192,7 @@ public:
                 throw invalid_argument("Digits allowed only!");
             }
         }
-    };
+    }
     BigDecimalInt(int DecInt) // overload for integer inputs
     {
         string DecStr = to_string(DecInt);
@@ -203,80 +206,98 @@ public:
         {
             num = "0";
         }
-    };
+    }
 
     BigDecimalInt operator+(BigDecimalInt anotherDec) // Not Complete
     {
-        int carry = 0;
-        bool switched = false;
-        BigDecimalInt biggest = (num.length() > anotherDec.num.length() ? *this : anotherDec);
-        BigDecimalInt smallest = (num.length() > anotherDec.num.length() ? anotherDec : *this);
+        BigDecimalInt first = *this;
+        BigDecimalInt second = anotherDec;
+        char tempSign[]{first.signChar, second.signChar};
+        char mainSign;
+        first.signChar = '+';
+        second.signChar = '+';
+        bool firstIsBigger = (first > second);
+        mainSign = tempSign[(firstIsBigger ? 0 : 1)];
+        if (firstIsBigger)
+        {
+            second.num = getLeadingZeros(first.num, second.num) + second.num;
+        }
+        else
+        {
+            first.num = getLeadingZeros(second.num, first.num) + first.num;
+        }
+        if ((first.signInt * second.signInt) != 1)
+        {
+            first.signChar = (firstIsBigger ? '+' : '-');
+            first.signInt = (firstIsBigger ? 1 : -1);
+            second.signChar = (firstIsBigger ? '-' : '+');
+            second.signInt = (firstIsBigger ? -1 : 1);
+        }
+        else
+        {
+            first.signInt = 1;
+            second.signInt = 1;
+        }
+
+        string sum;
+        int carry{0};
+        int value{0};
+        for (int i = first.num.length() - 1; i >= 0; i--)
+        {
+            value = carry + first.signInt * (first.num[i] - 48) + second.signInt * (second.num[i] - 48);
+            cout << carry << " + " << first.signInt << " * " << (first.num[i] - 48) << " + " << second.signInt << " * " << (second.num[i] - 48) << " = " << value << endl;
+            carry = 0;
+
+            if (i > 0)
+            {
+                if (value > 9)
+                {
+                    carry = 1;
+                    value %= 10;
+                }
+                else if (value < 0)
+                {
+                    carry = -1;
+                    value += 10;
+                }
+            }
+            sum = to_string(abs(value)) + sum;
+        }
+        sum = mainSign + removeLeadingZeros(sum);
+        return BigDecimalInt(sum);
+    }
+
+    string
+    getNum()
+    {
+        return removeLeadingZeros(((num == "0") ? "" : ((signChar == '+') ? "" : "-")) + num);
+    }
+
+private:
+    string getLeadingZeros(string s1, string s2)
+    {
         string zeros;
-        for (int i = 0; i < biggest.num.length() - smallest.num.length(); i++)
+        for (int i = 0; i < s1.length() - s2.length(); i++)
         {
             zeros += "0";
         }
-        if (biggest.signInt == -1)
+        return zeros;
+    }
+    string removeLeadingZeros(string s)
+    {
+        for (int i = 0; i < s.length() - 1; i++)
         {
-            biggest.signInt *= -1;
-            smallest.signInt *= -1;
-            switched = true;
-        }
-        smallest.num = zeros + smallest.num;
-        string sum;
-        int value;
-        for (int i = biggest.num.length() - 1; i >= 0; i--)
-        {
-            value = carry + biggest.signInt * (biggest.num[i] - 48) + smallest.signInt * (smallest.num[i] - 48);
-            carry = 0;
-            if (value >= 0)
+            if (s[0] == '0')
             {
-                if ((value > 9))
-                {
-                    carry = value / 10;
-                    sum = to_string(abs(value - (value / 10) * 10)) + sum;
-                }
-                else
-                {
-                    sum = to_string(abs(value)) + sum;
-                }
-            }
-            else
-            {
-                carry = -1;
-                sum = to_string(abs(value + 10)) + sum;
-            }
-            if (i == 0 && carry)
-            {
-                sum = to_string(abs(carry)) + sum;
-            }
-        }
-        for (int i = 0; i < sum.length(); i++)
-        {
-            if (sum[0] == '0')
-            {
-                sum = sum.erase(0, 1);
+                s = s.erase(0, 1);
             }
             else
             {
                 break;
             }
         }
-        if (switched)
-        {
-            biggest.signInt *= -1;
-            smallest.signInt *= -1;
-        }
-        sum = ((biggest.signChar == '+') ? "+" : "-") + sum;
-        return BigDecimalInt(sum);
+        return s;
     }
-
-    string getNum()
-    {
-        return ((signChar == '+') ? "+" : "-") + num;
-    }
-
-private:
     int getSign(string s)
     {
         if (s[0] == '-')
@@ -331,14 +352,12 @@ int main()
     BigDecimalInt num1("123456789012345678901234567890");
     BigDecimalInt num2("+113456789011345678901134567890");
     BigDecimalInt num3("-200000000000000000000000000000");
-    BigDecimalInt num4 = num1 + num2;
-    // BigDecimalInt num5 = num2 - num1;
-    cout << num1 + num2 << endl;
+    BigDecimalInt num4 = num2 + num1;
+    BigDecimalInt num5 = num2 - num1;
     cout << "num1 = " << num1 << endl;
     cout << "num2 = " << num2 << endl;
     // Next statement will print 236913578023691357802369135780
     cout << "num2 + num1 = " << num4 << endl;
-
     // Next statement will print -313456789011345678901134567890
-    // cout << "num2 - num1 = " << num5 << endl;
+    cout << "num2 - num1 = " << num5 << endl;
 }
